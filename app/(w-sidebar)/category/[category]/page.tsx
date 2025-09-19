1badf196c6a58ee6061ed9aec9265b393a4fa627
GabrielPTorres <126464973+GabrielPTorres@users.noreply.github.com>
2025-09-20 07:47:01 +0800

    buildfix3

--- app/(w-sidebar)/category/[category]/page.tsx
+++ app/(w-sidebar)/category/[category]/page.tsx
@@ -1,12 +1,10 @@
 import { use } from "react";
 import ItemGrid from "@/components/marketplace/item-grid";
 
-export default function CategoryPage({
-  params,
-}: {
-  params: Promise<{ category: string }>;
-}) {
-  const { category } = use(params);
+type TParams = Promise<{ category: string }>;
+
+export default function CategoryPage(props: { params: TParams }) {
+  const { category } = use(props.params);
 
   const formattedCategory = decodeURIComponent(category).replace(/-/g, " ");
 
